7742a1419b2360a1583b14b8dff1e35721c1f2fd
Brice <lebowvsky@gmail.com>
2022-12-15 19:22:15 +0100

    feat(init): :rocket: Init project

---
+++ src/components/ListComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>ListComponent</h2>
+  <ListItemCoponent v-for="item in nbr" :key="item" />
+</template>
+
+<script lang="ts" setup>
+import ListItemCoponent from "./ListItemCoponent.vue";
+const nbr = [1, 2, 3, 4, 5, 6, 7, 8];
+</script>
